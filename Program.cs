5d196d268b0bf3a3fe90de1086983b32329cb528
Théo Quintão <0000900198@senaimgaluno.com.br>
2024-10-01 10:15:18 -0300

    Release

--- Program.cs
+++ Program.cs
@@ -15,25 +15,29 @@ HashSet<int> numerosUnicos = new HashSet<int>();
 
 bool repetir;
 using (StreamWriter escrever = new StreamWriter("jogos-mega-sena.txt"))
-{
-        Console.Write("Deseja realizar quantos jogos: ");
-    if (int.TryParse(Console.ReadLine(), out int qtdJogoInformada))
+{   
+    do
     {
-        do
+        Console.Write("\nDeseja realizar quantos jogos: ");
+        if (int.TryParse(Console.ReadLine(), out int qtdJogoInformada))
         {
-            Console.Write("Informar a quantidade de dezena: ");
+            
+            Console.Write("\nInformar a quantidade de dezena: ");
             if (int.TryParse(Console.ReadLine(), out int qtdDezenaInformada))
             {
                 if (qtdDezenaInformada < 6 || qtdDezenaInformada > 15)
-                        repetir = true;
+                            repetir = true;
                     else
-                            repetir = false;
+                                repetir = false;
 
                 if (repetir == false)
                 {
                     for (int i = 0; i < qtdJogoInformada; i++)
                         {
+                            Console.ForegroundColor = ConsoleColor.Blue;
                             Console.Write($"\x0A Jogo {i + 1}: ");
+                            escrever.Write($"\x0A Jogo {i + 1}: ");
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
                             while (numerosUnicos.Count < qtdDezenaInformada)
                             {
                                 int numero = rnd.Next(1, 61);
@@ -44,46 +48,59 @@ using (StreamWriter escrever = new StreamWriter("jogos-mega-sena.txt"))
                             foreach (int a in Jogos)
                             {
                                 if (a == Jogos[0])
+                                {
                                     Console.Write($"{a:D2}");
+                                    escrever.Write($"{a:D2}");
+                                }  
                                 else
+                                {
                                     Console.Write($"-{a:D2}");
+                                    escrever.Write($"-{a:D2}");
+                                }
                             }
                             numerosUnicos.Clear();
                         }
-                        
-                    escrever.WriteLine();
-                    Console.WriteLine();
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Jogos gerados e salvos no arquivo 'jogos-mega-sena.txt'.\n");
-                    Console.ResetColor();
+                            
+                        escrever.WriteLine();
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\nJogos gerados e salvos no arquivo 'jogos-mega-sena.txt'.\n");
+                        Console.ResetColor();
                 }
                 else
                 {
-                    Console.WriteLine("Quantidade dezena menor que 6");
+                    Console.WriteLine("\nQuantidade dezena menor que 6");
                     repetir = true;
-
                 }
-            
             }    
             else
-                {
-                    repetir = true;
-                    Console.WriteLine("Número inválido!");
-                }
-        }while (repetir == true); 
-    }   
-    else
-    {
-            Console.WriteLine("Número inválido!");
-    }
+            {
+                repetir = true;
+                Console.WriteLine("\nNúmero inválido!");
+            }
+        
+        }   
+        else
+        {
+            repetir = true;
+            Console.WriteLine("\nNúmero inválido!");
+        }
+    }while (repetir == true);
 
     // Solicitar o valor do prêmio
 
+    repeat:
     Console.Write("Informe o valor do prêmio: ");
     if (float.TryParse(Console.ReadLine(), out float valorPremio))
     {
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine($"\nQuem acertar 6 números ganhará R$ {valorPremio * 0.75:F2}");
-        Console.WriteLine($"Quem acertar 5 números ganhará R$ {valorPremio * 0.15:F2}");
-        Console.WriteLine($"Quem acertar 6 números ganhará R$ {valorPremio * 0.10:F2}");
+        Console.WriteLine($"\nQuem acertar 5 números ganhará R$ {valorPremio * 0.15:F2}");
+        Console.WriteLine($"\nQuem acertar 6 números ganhará R$ {valorPremio * 0.10:F2}");
+    }
+    else
+    {
+        Console.WriteLine("\nNúmero inválido!");
+        goto repeat;
     }
 }
